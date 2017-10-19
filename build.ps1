048cd2685c1a7e05ea6ed09ec1eb309d68436c32
Jesse Rosenberger <git@jro.cc>
2017-10-19 13:33:37 +0300

    Restrict deploy to the correct tag.

--- build.ps1
+++ build.ps1
@@ -4,18 +4,10 @@ New-Item -Type Directory -Force $buildDirectory
 
 $nuspecFile = Join-Path $PSScriptRoot 'meteor.nuspec'
 
-# Get the version information out of this file.
-[xml]$nuspec = Get-Content -Path $nuspecFile
-$testPackageId = $nuspec.package.metadata.id
-$testVersion = $nuspec.package.metadata.version
-
 Write-Host "Installing chocolatey-core extensions..." -ForegroundColor Magenta
 & choco.exe install chocolatey-core.extension
 
 Write-Host "Running 'choco pack' for ${testVersion}..." -ForegroundColor Magenta
 & choco.exe pack $nuspecFile --outputdirectory $buildDirectory
-
-$nupkgFile = "${testPackageId}.${testVersion}.nupkg"
-$nupkgPath = Join-Path $buildDirectory $nupkgFile
 
 Get-ChildItem $buildDirectory
